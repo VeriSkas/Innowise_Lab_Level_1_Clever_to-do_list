077bf24d6d3ded9a960424ce4590d6aa28ebde61
VeriSkas <92_sveto4ka@mail.ru>
2022-12-20 21:48:25 +0300

    added .env file with config

--- to-do/src/api/api-config.js
+++ to-do/src/api/api-config.js
@@ -2,18 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
-export const databaseURL =
-  'https://clever-to-do-c6104-default-rtdb.europe-west1.firebasedatabase.app';
-
 const firebaseConfig = {
-  apiKey: 'AIzaSyDi7rXX5WKblCFwfEocHIOhV_-dkZiugH0',
-  authDomain: 'clever-to-do-c6104.firebaseapp.com',
-  databaseURL:
-    'https://clever-to-do-c6104-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'clever-to-do-c6104',
-  storageBucket: 'clever-to-do-c6104.appspot.com',
-  messagingSenderId: '1051467986788',
-  appId: '1:1051467986788:web:b87458f31873060cdb6c71',
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 export const app = initializeApp(firebaseConfig);
